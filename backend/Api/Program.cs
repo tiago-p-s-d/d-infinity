ee47ef268d5b5ade92e16bcc179ad104c8f442a4
tiagopsilvad <tiagopsilvad@gmail.com>
2026-02-21 22:41:19 -0300

    Adição do banco de dados e configurações iniciais

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -6,6 +6,14 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>{
+    options.AddDefaultPolicy(policy =>{
+        policy.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    })
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +25,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors();
 
 app.MapControllers();
 
